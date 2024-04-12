786ebef1d510154999056b3e9b8e1bbd7d74a7ff
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-12 07:50:20 +0200

    setters and getters of petition class

--- Sources/Petiton.cpp
+++ Sources/Petiton.cpp
@@ -1,1 +1,34 @@
+#include<iostream>
 #include "Petiton.h"
+#include <string>
+
+using namespace std;
+//Constructor
+
+
+//Setter Methods
+void Petition::setTitle(string Titl){
+    int len_title = Titl.length;
+    int iterator{0};
+    numofspaces{0};
+    if(len_title == 0){cout<<"Please Enter Title Correctly !!"}
+    for (; iterator<len_title; iterator++){
+        if (Titl[iterator] == " ") {
+            numofspaces+=1
+        }}
+    if (numofspaces < 2) {
+        cout<<"Please Enter 3 words at least for the Title !!"}
+    
+    else {Title = Titl}
+}
+
+
+void Petition::setContent(string Content) {Content=Conten;}
+
+
+//Getter Methods
+string getTitle(){return Title;}
+string getContent(){return Content;}
+
+
+
