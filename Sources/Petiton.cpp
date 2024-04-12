0c901ebc09694f21466bdedb160f9f17c4998b36
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-12 07:50:20 +0200

    setters and getters of petition class

--- Sources/Petiton.cpp
+++ Sources/Petiton.cpp
@@ -1,1 +1,20 @@
 #include "Petiton.h"
+#include <string>
+
+//Constructor
+Petition(string Titl, string Conten){
+    Title = Titl;
+    Content = Conten;
+}
+
+//Setter Methods
+void setTitle(string Titl){Title = Titl;}
+
+void setContent(string Content) {Content=Conten;}
+
+
+//Getter Methods
+string getTitle(){return Title;}
+string getContent(){return Content;}
+
+
