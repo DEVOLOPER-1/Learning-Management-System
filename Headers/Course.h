df4fbdef4d54ef30d3f8e7554e6007cacf3aa94f
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-14 01:08:50 +0200

    According to a fast discussion with mo. radwan some edits were done

--- Headers/Course.h
+++ Headers/Course.h
@@ -5,25 +5,25 @@ class Course
 {
 private:
 	string Title; 
-	vector<string> instructor; //setting courses codes wil be before setting course setting instructors as it will depend on the length of courses codes
-	vector<string> Code;
-	vector<string> Prerequisete;
-	int CreditHours;
-	vector<int> Section;
-	vector <string> TimeSlot;
+	string instructor; //setting courses codes wil be before setting course setting instructors as it will depend on the length of courses codes
+	string Code;
+	string Prerequisete;
+	unsigned int CreditHours;
+	unsigned int Section;
+	string TimeSlot;
 	string Day;
-	vector<string> Place;
+	string Place;
 public:
-	void setTitle(string);
-	void setInstructor(string,vector<string> Code);
-	void setCode(vector <string>);
+	void setTitle(string title);
+	void setInstructor(string Instructor_name);
+	void setCode(string course_code);
     //void CreditHours(unsigned int CreditHours, vector<string> Code);
-    void Course::setPrerequisete(vector<string>prerequisetes,vector<string> Code);
+    void Course::setPrerequisete(string prerequisetes);
     void setCreditHours(unsigned int section,vector<string> Code);
-	void setSection(vector <int> section,vector<string> Code);
-	void setPlace(vector <string> Placevector,vector<string> Code);
-	void setTime(vector<string> Timevector , vector <string> Code);
-	void setDay(vector<string> Dayvector , vector<string> Code);
+	void setSection(unsigned int sectionvector);
+	void setPlace(string Placevector);
+	void setTime(string time);
+	void setDay(string temp_day);
 	string getTitle();
 	Instructor getInstructor();
 	string getCode();
--- Sources/Course.cpp
+++ Sources/Course.cpp
@@ -29,7 +29,7 @@ void Course::setTitle(string title){
     else {Title = title;}
 }
 
-void Course::setInstructor(string Instructor_name,vector<string> Code){
+void Course::setInstructor(string Instructor_name){
     int len_name = Instructor_name.length();
     int iterator = 0;
     int num_of_spaces = 0;
@@ -43,21 +43,20 @@ void Course::setInstructor(string Instructor_name,vector<string> Code){
         }}
         if (num_of_spaces < 1) {
             cout<<"Please Enter The first and second name at least !!";}
-        else{for(int iterator1{0}; iterator1<Code.size();iterator1++){
-            cout<<"Enter the Instructor of "<<Code[iterator1]<<"  "; cin>>Instructor_name; instructor_names.push_back(Instructor_name); }}
-            vector <string> instructor = instructor_names;
-    }
+        else{instructor = Instructor_name;}}
 
-void Course::setCode(vector<string>course_code){
+void Course::setCode(string course_code){
+    /*
     unsigned int num_of_courses{0};
     string temp_course_code;
     cout<<"Enter No. of Course codes you will enter"; cin>>num_of_courses;
     for(int iterator{0}; iterator<num_of_courses;iterator++){
         cout<<"Enter Course Code : "; cin>>temp_course_code;
         course_code.push_back(temp_course_code);
-    } vector<string> Code = course_code;
+    }*/ string Code = course_code;
     }
-void Course::setPrerequisete(vector<string>prerequisetes,vector<string> Code){
+void Course::setPrerequisete(string prerequisetes){
+    /*
     int prerequisete_length = Code.size();
     string temp_prerequisete;
     for(int iterator{0}; iterator<prerequisete_length;iterator++){
@@ -68,54 +67,63 @@ void Course::setPrerequisete(vector<string>prerequisetes,vector<string> Code){
 void Course::setCreditHours(unsigned int CreditHours,vector<string> Code){
     int *array{ new int[Code.size()]};
     for(int iterator{0}; iterator<Code.size();iterator++){
-        cout<<"Enter Credit Hours :  ";  cout<<Code.at(iterator)<<"  "; cin>>array[iterator]; }
+        cout<<"Enter Credit Hours :  ";  cout<<Code.at(iterator)<<"  "; cin>>array[iterator]; } */
+Prerequisete = prerequisetes;
 }
-void Course::setSection(vector <int> sectionvector,vector<string> Code){
+void Course::setSection(unsigned int sectionvector){
     //int *section_array = new int[Code.size()];
+    /*
     int num_of_courses = Code.size();
     int temp_num_of_section {0};
     for(int iterator{0}; iterator<num_of_courses;iterator++){
-        cout<<"Enter Section :  "; cout<<Code.at(iterator)<<"  "<<"Section"; cin>>temp_num_of_section; sectionvector.push_back(temp_num_of_section);
-        if (temp_num_of_section == 0)
-        { cout<<"Invalid Entry you can't enter 0"; break;}
+        cout<<"Enter Section :  "; cout<<Code.at(iterator)<<"  "<<"Section"; cin>>temp_num_of_section; sectionvector.push_back(temp_num_of_section);*/
+        if (sectionvector == 0)
+        { cout<<"Invalid Entry you can't enter 0";}
+        Section = sectionvector;
         }
-    vector<int>Section = sectionvector;
+    
     //int Section[num_of_courses];   
     /*
     for (int iterator1{0}; iterator1 <num_of_courses; iterator1++)
-     {int Section[iterator1] = section_array[iterator1];}*/}
+     {int Section[iterator1] = section_array[iterator1];}*/
 
-void Course::setPlace(vector <string> Placevector,vector<string> Code){
+void Course::setPlace(string Placevector){
+    /*
     int num_of_courses = Code.size();
     string temp_place;
     for(int iterator{0}; iterator<num_of_courses;iterator++){
         cout<<"Enter Place :  "; cout<<Code.at(iterator)<<"  "; cin>>temp_place; Placevector.push_back(temp_place);
-        }
-    vector<string> Place = Placevector;}
+        }*/
+     Place = Placevector;}
 
-void Course::setDay(vector<string> Dayvector , vector<string> Code){
-    int num_of_courses = Code.size();
+void Course::setDay(string temp_day){
+    //int num_of_courses = Code.size();
     string temp_day;
+    /*
     for(int iterator{0}; iterator<num_of_courses;iterator++){
-        cout<<"Enter Day :  "; cout<<Code.at(iterator)<<"  "; cin>>temp_day; Dayvector.push_back(temp_day);
+        cout<<"Enter Day :  "; cout<<Code.at(iterator)<<"  "; cin>>temp_day; Dayvector.push_back(temp_day); */
         if (temp_day == "nan" || temp_day == " " or temp_day  == "")
-        { cout<<"Invalid Entry"; break;} }
-    vector<string> Day = Dayvector;}
+        { cout<<"Invalid Entry";} 
+        Day = temp_day;}
 
-void Course::setTime(vector<string> Timevector , vector <string> Code){
-int len_of_classes_vector = Code.size();
+void Course::setTime(string time){
+//int len_of_classes_vector = Code.size();
 string time[7]{"12.00PM"};
 string time_sample {"12.00PM"};
 cout<<"You will have to enter 7 slots to express the time from left to right like the following  "<<time_sample<<endl;
-
+/*
  for (int iterator0 = 0; iterator0 < len_of_classes_vector; iterator0++){
         for (int time_index{0}; time_index < 7; time_index++){
     cout<<time[time_index]<<time[time_index+1]<<endl;
     cout<<"Enter slot with index "<<time_index+1<<" "; cin>>time[time_index];} 
     Timevector.push_back(time[iterator0]);
-}
-TimeSlot = Timevector;
-}
+} */
+for (int time_index{0}; time_index < 7; time_index++){
+    cout<<time[time_index]<<time[time_index+1]<<endl;
+    cout<<"Enter slot with index "<<time_index+1<<" "; cin>>time[time_index];} 
+     
+    TimeSlot = time;
+} //This Method is not mature yet!!!
 
 
 
