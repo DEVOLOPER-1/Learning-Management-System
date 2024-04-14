f26f217baed44b0c9a5b052515c8870242d19dec
Youssef Mohammad <s-youssef.hameid@zewailcity.edu.eg>
2024-04-14 21:35:34 +0200

    Department Class under construction

--- Headers/Department.h
+++ Headers/Department.h
@@ -14,10 +14,23 @@ private:
 	vector <Instructor> Staff;
 	string Dean_Name;
 public:
-	void setDean(Dean);
+//Constructors____________________________________________________________________
+
+Department();
+
+
+//Setters__________________________________________________________________________
+	void setDean(string);
 	void setName(string);
 	void addMajor(string);
 	void addToStuff(Instructor);
 	void display();
+
+
+//Getters__________________________________________________________________________
+	string getName();
+	string getDean_Name();	
+	vector <string> getMajors();
+	vector <Instructor> getStaff();
 };
 
--- Sources/Department.cpp
+++ Sources/Department.cpp
@@ -3,6 +3,7 @@
 #include "Instructor.h"
 #include "Dean.h"
 #include "Department.h"
+<<<<<<< HEAD
 
 void Department::setDean(Dean) {
 
@@ -19,3 +20,53 @@ void Department::addToStuff(Instructor) {
 void Department::display() {
 
 }
+=======
+#include "Instructor.h"
+#include <string>
+#include <vector>
+using namespace std;
+
+//Constructors____________________________________________________________________
+
+
+
+
+
+
+
+//Setters____________________________________________________________________________
+void Department::setName(string name) {
+  for (int iterator = 0; iterator < name.size() - 1; iterator++) {
+    if (name[iterator] == ' ') {
+      Name = name;
+    }
+  }
+}
+
+
+void Department::setDean(string dean_name){
+
+  for (int iterator = 0; iterator < dean_name.size() - 1; iterator++) {
+    if (dean_name[iterator] == ' ') {
+      Dean_Name = dean_name;
+    }
+  }
+}
+
+
+void Department::
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Getters________________________________________________________________________
+>>>>>>> 78070ff (Department Class under construction)
