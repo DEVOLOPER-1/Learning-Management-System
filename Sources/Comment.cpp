5600d34a2af68b920eac750c8744a50612306266
Mohamed Radwan <1mohammed.radwan@gmail.com>
2024-04-14 22:45:18 +0200

    make progress in Comment class

--- Sources/Comment.cpp
+++ Sources/Comment.cpp
@@ -1,13 +1,47 @@
 #include "Comment.h"
-
+#include <ctime>
+#include <string>
 // Constructors
 
 
 // Seters
 
 
+void Comment::setDate()
+{
+    string CurrentDay, CurrentMonth, CurrentYear, CurrentDate;
+    time_t t = time(0);                 // get current time
+    tm* CurrentTimePtr = localtime(&t);
+    CurrentDay = CurrentTimePtr->tm_mday;
+    CurrentMonth = (CurrentTimePtr->tm_mon + 1) + "/";
+    CurrentYear = (CurrentTimePtr->tm_year + 1900) + "/";
+    CurrentDate = CurrentDay + CurrentMonth + CurrentYear;
+}
+
+void Comment::CheckPrivate()
+{
+}
+
+void Comment::setContent(string Content)
+{
+	this->Content = Content;
+}
+
+void Comment::reply(Comment)
+{
+}
+
+vector<Comment> Comment::getReplies()
+{
+	return vector<Comment>();
+}
+
 // Geters
-string Comment::getComment() const {return Comment;}
+string Comment::getContent() const {return Content;}
+string Comment::getDate() const{return Date;}
+void Comment::display(Student*, Instructor*, int, int) const
+{
+}
 vector<Comment> Comment::getReplies() const {return Replies;}
 // Other
 
