96c12a489272aa3daf72671061d414679b08ed44
Mohamed Radwan <1mohammed.radwan@gmail.com>
2024-05-17 08:08:24 +0300

    getter and setter for Comment class

--- Sources/Comment.cpp
+++ Sources/Comment.cpp
@@ -9,30 +9,18 @@ Comment::Comment(string content, string date, bool isPrivate) : content(content)
 }
 
 // Seters
-void Comment::setContent(string)
-{
-}
+void Comment::setContent(string content) { this->content = content; }
 
 // Geters
-string Comment::getContent() const
-{
-}
+string Comment::getContent() const { return content; }
 
-string Comment::getDate() const
-{
-	return string();
-}
+string Comment::getDate() const { return date; }
 
 
 // reply
-void Comment::reply(Comment)
-{
-}
+void Comment::reply(Comment r) { replies.push_back(r); }
 
-vector<Comment> Comment::getReplies()
-{
-	return vector<Comment>();
-}
+vector<Comment> Comment::getReplies() { return replies; }
 
 //others
 void Comment::display(Student*, Instructor*, int, int) const
@@ -41,4 +29,5 @@ void Comment::display(Student*, Instructor*, int, int) const
 
 void Comment::CheckPrivate()
 {
+
 }
