10f8ad4ac7e9d53b96f6a8e05d9abe0de5cd7d57
Mohamed Mourad <mohamedmoradmagdy1000@gmail.com>
2024-05-18 17:09:23 +0300

    xwedigets

--- Sources/trialGui.cpp
+++ Sources/trialGui.cpp
@@ -1,10 +1,36 @@
-// https://github.com/qPCR4vir/nana-docs/wiki/Getting-Started
+#include <wx/wxprec.h>  // For precompiled headers
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
 
-// #include <nana/gui/wvl.hpp>
-#include <nana/gui.hpp>
-int main()
-{
-    nana::form fm;
-    fm.show();
-    nana::exec();
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit() override {
+        // Create and show the main frame (window)
+        MyFrame* frame = new MyFrame();
+        frame->Show(true);
+        return true;
+    }
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame() : wxFrame(nullptr, -1, "My Button App") {
+        // Create a button
+        wxButton* button = new wxButton(this, wxID_ANY, "Click Me!");
+
+        // Connect the button click event to a function
+        button->Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this);
+    }
+
+private:
+    void OnButtonClick(wxCommandEvent& event) {
+        // This function will be called when the button is clicked
+        wxMessageBox("The button was clicked!");
+    }
+};
+
+int main() {
+    wxApp application;  // Create an instance of the application object
+    return wxApp::MainLoop();  // Start the event loop and run the application
 }
